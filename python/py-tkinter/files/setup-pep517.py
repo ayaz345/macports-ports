777e6428486e724ca9a69c979e7543ad2e504a3c
Sourcery AI <>
2023-06-23 09:55:28 +0000

    'Refactored by Sourcery'

--- python/py-tkinter/files/setup-pep517.py
+++ python/py-tkinter/files/setup-pep517.py
@@ -3,14 +3,14 @@ from setuptools import Extension, setup
 tkversion = "__TK-VERSION__"
 
 setup(
-    ext_modules = [
+    ext_modules=[
         Extension(
             name="_tkinter",
             sources=["_tkinter.c", "tkappinit.c"],
             define_macros=[("WITH_APPINIT", 1)],
-            include_dirs = ["__PREFIX__/include"],
-            libraries = ["tcl" + tkversion, "tk" + tkversion],
-            library_dirs = ["__PREFIX__/lib"]
+            include_dirs=["__PREFIX__/include"],
+            libraries=[f"tcl{tkversion}", f"tk{tkversion}"],
+            library_dirs=["__PREFIX__/lib"],
         )
     ]
 )
